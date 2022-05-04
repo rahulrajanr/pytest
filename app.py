435a5ed61ccbbd929636b0f193aa4d33343e9f6d
Rahul Rajan <61702990+rahulrajanr@users.noreply.github.com>
2022-05-04 11:34:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome!webhook works build 12"
+    return "Welcome!webhook works build 13"
 
 @app.route('/how are you')
 def hello():
     return 'I am good, how about you?'
 
-
+@app.route('/file')
+def read_file():
+    f = open("/data/testfile.txt")
+    contents = f.read()
+    return contents
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
