d4cdf03f1d131dfea76dadad688fe6b4153d8739
Rahul Rajan <61702990+rahulrajanr@users.noreply.github.com>
2022-05-04 11:28:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,7 @@ def main():
 def hello():
     return 'I am good, how about you?'
 
-@app.route('/file')
-def read_file():
-    f = open("/data/testfile.txt")
-    contents = f.read()
-    return contents
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
